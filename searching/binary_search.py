b4acd9e3f47700ec82a0d2079fcea10228361f4a
Steven Turner <81867253+steveturner1183@users.noreply.github.com>
2023-05-20 16:29:34 -0400

    Complete binary search

--- searching/binary_search.py
+++ searching/binary_search.py
@@ -1,2 +1,17 @@
 def binary_search(arr, value):
-    return 6
+    low = 0
+    high = len(arr) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+
+        if arr[mid] == value:
+            return mid
+
+        elif value > arr[mid]:
+            low = mid
+
+        elif value > arr[mid]:
+            high = mid
+
+    return -1
